b76458081f0ae2cc6bf2761397dba87a7a1a4e06
AbelMaireg <abelrighthere@gmail.com>
2025-07-02 10:27:37 +0300

    build(lint)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,8 +15,8 @@ const eslintConfig = [
     rules: {
       'react/no-unescaped-entities': 'off',
       '@next/next/no-page-custom-font': 'off',
-      'react-hooks/exhaustive-deps': 'off',
-      '@next/next/no-image-element': 'off'
+      'react-hooks/exhaustive-deps': 'error',
+      '@next/next/no-img-element': 'off'
     },
   }),
 ]
